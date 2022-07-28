5cc0918a978391298dfc5b08ec3e5474d926e28f
GaboSLV <gabosv23@gmail.com>
2022-07-28 19:59:00 -0300

    Simulador interactivo

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@ let salir;
 //Funcionamiento de apuesta.
 function moneda () {
   monto = parseInt(prompt("Ingresa la cantidad que deseas apostar"));
-  balance = balance - monto;
+  if (monto>balance) {
+    alert("Lo lamentamos, no tienes saldo para realizar esta operación")
+  }
+  else {
+  balance -= monto;
   alert("Tu balance actual es: " + balance);
   solucion = Math.random();
   console.log(solucion);
-  if (solucion >= 0.5) {
-balance = balance + (monto*1.9);
+  if (solucion <= 0.5) {
+balance += (monto*1.9);
     alert("¡Felicidades! Ganaste, tu balance ahora es: " + balance);
   } else {
     alert("Lo lamentamos perdiste, tu balance actual es: " + balance);
@@ -25,7 +29,7 @@ balance = balance + (monto*1.9);
   if (balance <=0) {
     alert ("Lo lamentamos, no tienes balance para realizar esta operación. Recarga tu balance.")
   }
-}
+}}
 
 
 
@@ -34,6 +38,7 @@ balance = balance + (monto*1.9);
 
 //Bucle de apuesta
 balance = 50000;
+alert("Dispones de $50.000 de balance")
 do {
  moneda ();
 } while (salir != "n");
